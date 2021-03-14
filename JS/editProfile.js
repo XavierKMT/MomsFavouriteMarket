600a0ff28ee208bf52408b4655fc99138a65be92
Divleen12 <divleenkahluwalia@gmail.com>
2021-03-14 12:39:40 -0400

    changed files + JS

---
+++ JS/editProfile.js
@@ -0,0 +1,172 @@
+user_email=document.getElementById('inputEmail4');
+user_new_email=document.getElementById('inputEmail');
+user_phone=document.getElementById('phone');
+let email_expression = /[\w]+@[\w.]+/;
+
+
+/*The check_email function checks the Current Email field to ensure that the user enters an ID that is valid. 
+If invalid, it displays an error message to the user*/
+let check_email=function(){   
+if((user_email.value=="") && (user_new_email.value=="")){
+    document.getElementById("mismatch2").style.display="none";
+    document.getElementById("mismatch").style.display="none";
+    return true;
+}
+    else if(!(user_email.value.match(email_expression))){
+        document.getElementById("mismatch").style.display="inline";
+        return false;
+     }
+    
+     else{
+         document.getElementById("mismatch").style.display="none";
+         return true;
+     }
+}
+
+/**The check_new_email function ensures that the new email is a valid expression. It also checks if the user has entered an email in the 
+ * Current Email field. If not, an error message is displayed.
+ */
+let check_new_email=function(){   
+   
+            if((user_email.value=="") && (user_new_email.value=="")){
+                document.getElementById("mismatch2").style.display="none";
+                document.getElementById("mismatch").style.display="none";
+                return true;
+            }
+        else if(!(user_new_email.value.match(email_expression))){
+           
+            document.getElementById("mismatch2").style.display="inline";
+            return false;
+            
+         }
+         else if(user_email.value==""){
+            document.getElementById("mismatch2").style.display="inline";
+            return false;
+         }
+         else{
+             document.getElementById("mismatch2").style.display="none";
+             return true;
+         }
+    }
+
+user_email.onblur=check_email;
+user_new_email.onblur=check_new_email;
+
+
+/** User password values (both new and old) are stored in the variables as shown below. The check_password function ensures that both the old and the new 
+ * passwords entered are the same. If not, an error message is displayed. This function checks for the above as soon as the password field moves out of focus.
+ */
+user_password=document.getElementById('inputPassword4');
+user_new_password=document.getElementById('inputPassword');
+
+
+let check_password=function(){
+    if(!(user_password.value.localeCompare(user_new_password.value)==0)){
+        document.getElementById("mismatch_password").style.display="inline";
+        return false;
+     }
+
+     if((user_password.value=="")||(user_new_password.value=="")){
+        document.getElementById("mismatch_password").style.display="none";
+        return false;
+     }
+     document.getElementById("mismatch_password").style.display="none";
+     return true;
+}
+
+user_new_password.onblur=check_password;
+
+
+/**
+ * The phone_number function checks if the user enters a valid phone number- one with 10 digits and without any special characters. An error message is displayed 
+ * accordingly.
+ */
+let phone_number= function(){
+    let phone=/^\d{10}$/;
+    if(!(user_phone.value.match(phone))&& (user_phone.value!="")){
+        document.getElementById("mismatch_phone").style.display="inline";
+        return false;
+    }
+    document.getElementById("mismatch_phone").style.display="none";
+    return true;
+}
+
+user_phone.onblur=phone_number;
+
+
+/**
+ * The resetCheck function helps reset the form once all the input fields have been validated. It is called from the submission function and resets the 
+ * entire form after submission. The preventDefault function associated with the submitButton variable ensures that the default action of the button 
+ * (that is, to send the entered values somewhere) is prevented.
+ */
+
+let submitButton=document.getElementById('save');
+let inputs=document.querySelectorAll('input');
+
+
+let resetCheck=function(){
+    for(i=0; i<inputs.length; i++){
+        inputs[i].value='';
+    }
+    // inputs[inputs.length-1].checked=false;
+    document.getElementById("mismatch").style.display="none";
+    document.getElementById("mismatch2").style.display="none";
+    document.getElementById("mismatch_password").style.display="none";
+    document.getElementById("mismatch_password").style.display="none";
+    document.getElementById("mismatch_phone").style.display="none";
+    document.getElementById("province").selectedIndex=0;
+}
+
+
+
+submitButton.addEventListener('click', function(e)
+{
+    e.preventDefault();
+});
+    
+
+/**The submission function checks all the fields once more to ensure that the user not entered invalid details or left some field incomplete. It gives either
+ * an error message or a completion message accordingly.
+ */
+function submission()
+{   
+   
+    if(((user_email.value!="")&&(user_new_email.value==""))||((user_email.value=="")&&(user_new_email.value!="")))
+    {
+        
+        email = "Email";
+    }
+    else if((!(user_new_email.value.match(email_expression))||!(user_email.value.match(email_expression)))&& (user_email.value!="") && (user_new_email.value!="")){
+        email="Email";
+    }
+    else{
+        email="";
+    }
+
+    if(((user_password.value!="")&&(user_new_password.value==""))||((user_password.value=="")&&(user_new_password.value!="")))
+    {
+        password = "Password";
+    }
+    else if(!(user_password.value.localeCompare(user_new_password.value)==0)){
+        password = "Password";
+    }
+    else{
+        password="";
+    }
+    
+    if(!phone_number()){
+        phone="Phone Number";
+    }
+    else{
+        phone="";
+    }
+    
+    if((email!="")||(password!="") || (phone!="")){
+        swal("Please ensure the following field(s) are correctly filled:\n\n"+ email + "\n " + password + "\n"+ phone);
+    }
+    else{
+        swal("Done!", "The changes were saved successfully!", "success");
+        resetCheck();
+    }
+    
+}
---
+++ JS/signupvalidation.js
@@ -0,0 +1,152 @@
+user_email=document.getElementById('inputEmail');
+user_confirm_email=document.getElementById('confEmail');
+user_phone=document.getElementById('phone');
+let email_expression = /[\w]+@[\w.]+/;      //This is the regular expression the email is compared against.
+
+/**The check_email function checks the Email fields to ensure that both the IDs entered are the same and are also valid.
+If invalid, it displays an error message to the user. The check_email_expression function ensures that the ID that the user enters in the first
+email field is a correct email expression, else it alerts the user about the same.*/
+let check_email=function(){   
+ 
+    if(!(user_email.value.localeCompare(user_confirm_email.value)==0)){
+        document.getElementById("mismatch").style.display="inline";
+        return false;
+     }
+     else if((user_email.value=="")||(user_confirm_email.value=="")){
+        document.getElementById("mismatch").style.display="none";
+        document.getElementById("mismatch2").style.display="none";
+        return false;
+     }
+     else{
+         document.getElementById("mismatch").style.display="none";
+         return true;
+     }
+    
+}
+
+let check_email_expression=function(){
+        if(!(user_email.value.match(email_expression))&& (user_email.value!="")){
+        
+        document.getElementById("mismatch2").style.display="inline";
+        return false;
+      
+     }
+     document.getElementById("mismatch2").style.display="none";
+
+}
+
+user_email.onblur=check_email_expression;
+user_confirm_email.onblur=check_email;
+
+
+/** The phone_number function validates the phone number to ensure that there are no special characters and that the digits entered are all numbers. */
+
+let phone_number= function(){
+    let phone=/^\d{10}$/;
+    if(!(user_phone.value.match(phone)) && (user_phone.value!="")){
+        document.getElementById("mismatch_phone").style.display="inline";
+        return false;
+    }
+    document.getElementById("mismatch_phone").style.display="none";
+    return true;
+}
+
+user_phone.onblur=phone_number;
+
+/** check_password() makes sure that both the password fields have the same characters, else an error message is shown. If both the fields are empty, 
+ * no message is displayed.
+ */
+user_password=document.getElementById('inputPassword');
+user_confirm_password=document.getElementById('confPassword');
+let check_password=function(){
+    
+    if(!(user_password.value.localeCompare(user_confirm_password.value)==0)){
+        document.getElementById("mismatch_password").style.display="inline";
+        return false;
+     }
+
+     if((user_password.value=="")||(user_confirm_password.value=="")){
+        document.getElementById("mismatch_password").style.display="none";
+        return false;
+     }
+     document.getElementById("mismatch_password").style.display="none";
+     return true;
+}
+
+user_confirm_password.onblur=check_password;
+
+
+/**The resetCheck function resets the form once all the input fields have been validated. It is called from the submission function. The preventDefault 
+ * function associated with the submitButton variable ensures that the default action of the button 
+ * (that is, to send the entered values somewhere) is prevented. */
+let submitButton=document.getElementById('signup-submit');
+let resetButton=document.getElementById("signup-reset");
+let inputs=document.querySelectorAll('input');
+
+
+let resetCheck=function(){
+    for(i=0; i<inputs.length; i++){
+        inputs[i].value='';
+    }
+    inputs[inputs.length-1].checked=false;
+    document.getElementById("subscription_box_1").checked=false;
+    document.getElementById("subscription_box_2").checked=false;
+    document.getElementById("mismatch").style.display="none";
+    document.getElementById("mismatch2").style.display="none";
+    document.getElementById("mismatch_password").style.display="none";
+    document.getElementById("mismatch_phone").style.display="none";
+    document.getElementById("province").selectedIndex=0;
+   
+}
+
+submitButton.addEventListener('click', function(e)
+{
+    e.preventDefault();
+   
+});
+    
+/**The submission function checks all the fields once more to ensure that the user not entered invalid details or left some field incomplete. It gives either
+ * an error message or a completion message accordingly.
+ */
+function submission()
+{   
+    
+    if(!check_email() || !check_password() || !phone_number()){
+        if(!check_email()){
+            email="Email";
+        }
+        else{
+            email="";
+        }
+        if(!check_password()){
+            password="Password";
+        }
+        else{
+            password="";
+        }
+        if(!phone_number()){
+            phone="Phone Number";
+        }
+        else{
+            phone="";
+        }
+
+        swal("Make sure the following field(s) are correctly filled before submitting:\n\n"+ email + "\n " + password + "\n"+ phone);
+    }
+    else{
+        var check=document.getElementById('radioCheck').checked;
+        if(check==false){
+            swal("Checking the radio button before submitting is mandatory");
+        }
+        else
+        {
+            swal("Done!", "Your form was submitted successfully!", "success");
+            resetCheck();
+        }
+    }
+    
+};
+resetButton.addEventListener('click', resetCheck);
+
+
+
