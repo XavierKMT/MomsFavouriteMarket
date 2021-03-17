98e0a8c7be56514af66b223c8572dec07c090301
XavierKMT <78317478+XavierKMT@users.noreply.github.com>
2021-03-16 20:48:43 -0400

    Update cartScript.js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -709,8 +709,6 @@ function displayCart() {
     let cartList = document.getElementById('myTableCart');
     if (cartSize == NaN) {
         cartList.innerHTML += `
-        <br>
-        <br>
         <tr>
             <th colspan="5" class="text-center table-dark"
                     style="border-radius: 20px; border-color:white; padding:3px; background-color:#334754;">
