455fbe812e0f871ef3e7bd1bb40da76f8978ee46
Christina Darstbanian <minasderstep@gmail.com>
2021-04-19 15:24:53 -0400

    Update careers.php

--- php/careers.php
+++ php/careers.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mom's Favourite Market</title>
-    <link rel="shortcut icon" href="Images/logo.png">
+    <link rel="shortcut icon" href="../Images/logo.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -12,14 +12,14 @@
 
     <script src="https://kit.fontawesome.com/9cec6e31d9.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="CSS/main.css">
-    <link rel="stylesheet" href="CSS/cartStyle.css">
+    <link rel="stylesheet" href="../CSS/main.css">
+    <link rel="stylesheet" href="../CSS/cartStyle.css">
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="php/index.php"><img src="Images/logo.png" class="img-fluid"
+            <a class="navbar-brand" href="php/index.php"><img src="../Images/logo.png" class="img-fluid"
                     alt="Responsive image"><span id="title">Mom's Favourite Market</span>
             </a>
             <!--LINK TO MAIN PAGE-->
@@ -30,7 +30,7 @@
             <div class="collapse navbar-collapse" id="main-navigation">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="php/index.php" class="nav-link">Home</a>
+                        <a href="../index.php" class="nav-link">Home</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -38,32 +38,32 @@
                             Aisles
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="php/Dairy&Eggs_Relevance.php">Dairy & Eggs</a></li>
-                            <li><a class="dropdown-item" href="php/F&V_Relevance.php">Fruits & Vegetables</a></li>
-                            <li><a class="dropdown-item" href="php/Bakery_Relevance.php">Bakery</a></li>
-                            <li><a class="dropdown-item" href="php/Butchery_Relevance.php">Butchery</a></li>
-                            <li><a class="dropdown-item" href="php/Pantry_Relevance.php">Non-Perishable Food</a></li>
-                            <li><a class="dropdown-item" href="php/Frozen_Relevance.php">Frozen Food</a></li>
+                            <li><a class="dropdown-item" href="Dairy&Eggs_Relevance.php">Dairy & Eggs</a></li>
+                            <li><a class="dropdown-item" href="F&V_Relevance.php">Fruits & Vegetables</a></li>
+                            <li><a class="dropdown-item" href="Bakery_Relevance.php">Bakery</a></li>
+                            <li><a class="dropdown-item" href="Butchery_Relevance.php">Butchery</a></li>
+                            <li><a class="dropdown-item" href="Pantry_Relevance.php">Non-Perishable Food</a></li>
+                            <li><a class="dropdown-item" href="Frozen_Relevance.php">Frozen Food</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="php/WeeklyDeals.php" class="nav-link">Weekly Deals!</a>
+                        <a href="WeeklyDeals.php" class="nav-link">Weekly Deals!</a>
                     </li>
                     <li class="nav-item">
-                        <a href="php/environment1.php" class="nav-link">Environment</a>
+                        <a href="environment1.php" class="nav-link">Environment</a>
                     </li>
                     <li class="nav-item">
-                        <a href="php/careers.php" class="nav-link">Careers</a>
+                        <a href="careers.php" class="nav-link">Careers</a>
                     </li>
                     <li class="nav-item">
-                        <a href="php/About us.php" class="nav-link">About Us</a>
+                        <a href="About us.php" class="nav-link">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="php/loginpage.php" class="nav-link"><i class="far fa-user-circle fa-lg"></i> Sign in</a>
+                        <a href="loginpage.php" class="nav-link"><i class="far fa-user-circle fa-lg"></i> Sign in</a>
                     </li>
         
                     <li class="nav-item">
-                        <a href="php/Cart.php" class="nav-link"><i class="fas fa-shopping-cart fa-lg"></i></a>
+                        <a href="Cart.php" class="nav-link"><i class="fas fa-shopping-cart fa-lg"></i></a>
                     </li>
                 </ul>
             </div>
@@ -293,7 +293,7 @@
       <div class=" col-lg-4">
         <h5 class="text-center">Resources</h5>
         <ul class="list-unstyled text-small text-center">
-          <li><a class="text-muted" href="php/contact_us.php"><span style="color: white;">Contact Us
+          <li><a class="text-muted" href="contact_us.php"><span style="color: white;">Contact Us
           <li><a class="text-muted" href="#"><span style="color: white;">About Us</span></a></li>
           <!--IGA 5600 Boul Henri-Bourassa O, Montréal, QC H4R 0B8-->
           <li><a class="text-muted"
