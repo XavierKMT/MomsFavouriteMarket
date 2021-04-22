cf83d3ebb74cf883b8e8a68d112858bdbc4fc147
RosalieYelle <78310125+RosalieYelle@users.noreply.github.com>
2021-04-22 12:48:19 -0400

    Add files via upload

---
+++ php/headerProductList.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mom's Favourite Market</title>
+    <link rel="shortcut icon" href="../Images/logo.png">
+
+    <!--Bootstrap CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/9cec6e31d9.js" crossorigin="anonymous"></script>
+
+
+    <link rel="stylesheet" href="../CSS/main.css">
+    <link rel="stylesheet" href="../CSS/cartStyle.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/ProductListStyle.css">
+
+    <!--imports the javascript code-->
+    <script src= "../JS/ProductPageButtons.js"></script>
+</head>
+
+<body>
+    <header>
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="successadmin.php"><img src="../Images/logo.png"><span id="title">Mom's Favourite
+                        Market (ADMIN)</span></a></a>
+                <!--LINK TO MAIN PAGE-->
+                <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
+                    data-target="#main-navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="main-navigation">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="ProductList.php" class="nav-link">Product List</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="backstoreUserAccounts.php" class="nav-link">User List</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="orderLists.php" class="nav-link">Order List</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="loginpage2.php" class="nav-link"><i class="far fa-user-circle fa-lg"></i> Sign in</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+
+        </header>
