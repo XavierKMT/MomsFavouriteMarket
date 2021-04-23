56b058495a79e84199e9a844bee79d401c77469c
RosalieYelle <78310125+RosalieYelle@users.noreply.github.com>
2021-04-22 23:57:47 -0400

    Update organizingProduct_FE.php

--- php/organizingProduct_FE.php
+++ php/organizingProduct_FE.php
@@ -43,57 +43,6 @@ $productList = file_get_contents("../data/newProdList.txt")."</br>";
             array_push($pantry, $products[$i]);
     }
 
-  //  foreach($bakery as $key => $value){
-  //      echo "Key: $key; Value: $value <br />";
-  //  }
-/*
-$fruits = array();
-$veggies = array(); 
-$bagels = array();
-$breads = array();
-$pastries = array();
-$meat = array();
-$poultry = array();
-$seafood = array();
-$CY = array();
-$eggs = array();
-$MCB = array();
-$BI = array();
-$CF = array();
-$frozen = array();
-
-for($i = 0; $i<$num; $i++){
-    if($subAisle[$i] == "Fruits")
-        array_push($fruits, $products[$i]);
-    else if($subAisle[$i] == "Vegetables")
-        array_push($veggies, $products[$i]);
-    else if($subAisle[$i] == "Bagels")
-        array_push($bagels, $products[$i]);
-    else if($subAisle[$i] == "Breads")
-        array_push($breads, $products[$i]);
-    else if($subAisle[$i] == "Pastries")
-        array_push($pastries, $products[$i]);
-    else if($subAisle[$i] == "Meat")
-        array_push($meat, $products[$i]);
-    else if($subAisle[$i] == "Poultry")
-        array_push($poultry, $products[$i]);
-    else if($subAisle[$i] == "Seafood")
-        array_push($seafood, $products[$i]);
-    else if($subAisle[$i] == "Cheese & Yogurt")
-        array_push($CY, $products[$i]);
-    else if($subAisle[$i] == "Eggs")
-        array_push($eggs, $products[$i]);
-    else if($subAisle[$i] == "Milk, Cream & Butter")
-        array_push($MCB, $products[$i]);
-    else if($subAisle[$i] == "Baking Ingredients")
-        array_push($BI, $products[$i]);
-    else if($subAisle[$i] == "Canned Food")
-        array_push($CF, $products[$i]);
-    else if($subAisle[$i] == "Frozen")
-        array_push($frozen, $products[$i]);
-    
-}
-*/
 //See if it's in the weekly deal
 $sales = array();
 $noSales = array();
